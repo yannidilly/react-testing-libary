fb54c3c7ac34b38e22cfac53218c4bc0db6f2a17
Yanni <yannidilly.prog@gmail.com>
2022-10-21 19:11:08 -0300

    req2, implementação de todos os testes para o componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,8 +7,12 @@ describe('Realiza os testes do componente About', () => {
     render(<About />);
     const aboutTittle = screen.queryByRole('heading', { level: 2, name: 'About Pokédex' });
     expect(aboutTittle).toBeInTheDocument();
+    const paragrafo1 = screen.queryByText(/This application simulates a Pokédex/i);
+    const paragrafo2 = screen.queryByText(/One can filter Pokémons by type/i);
+    expect(paragrafo1).toBeInTheDocument();
+    expect(paragrafo2).toBeInTheDocument();
+
     const pokedexImage = screen.queryByRole('img', { name: 'Pokédex' });
-    console.log(pokedexImage.src);
     expect(pokedexImage).toBeInTheDocument();
     expect(pokedexImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
