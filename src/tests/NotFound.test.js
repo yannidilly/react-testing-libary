9b382a0ec54b65b766e5a82be013de5de3ff4084
Yanni <yannidilly.prog@gmail.com>
2022-10-21 20:24:26 -0300

    req4, implementa testes para componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+
+describe('Realiza testes na página Not Found', () => {
+  test('Testa se o conteúdo da página Not Found é igual ao esperado', () => {
+    render(<NotFound />);
+    const notFoundTitle = screen.queryByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(notFoundTitle).toBeInTheDocument();
+    const notFoundImage = screen.queryByRole('img', { name: 'Pikachu crying because the page requested was not found' });
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
