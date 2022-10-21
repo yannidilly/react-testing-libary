c396be5d02d6d0ca52554d51953ccc8598ca1c52
Yanni <yannidilly.prog@gmail.com>
2022-10-21 17:09:08 -0300

    req1, iniciando projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente App', () => {
+  test('Testa se o link Home encaminha para a página Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.queryByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Testa se o link About encaminha para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.queryByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa se o link Favorite Pokémons encaminha para a página Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemonsLink = screen.queryByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+    userEvent.click(favoritePokemonsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Testa se renderiza a página Not Found quando está em uma rota desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pagina-desconhecida');
+    });
+    const notFoundTitle = screen.queryByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
